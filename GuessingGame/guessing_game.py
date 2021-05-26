408d98a4498a06abf8da8b27d37bf877b9534dc8
Rodrigo Câmara <rodrigoarenacamara@gmail.com>
2021-05-26 14:10:18 -0300

    Adding points Adding difficulty
    Signed-off-by: Rodrigo Câmara <rodrigoarenacamara@gmail.com>

--- GuessingGame/guessing_game.py
+++ GuessingGame/guessing_game.py
@@ -1,15 +1,29 @@
+import random
+
+random.seed()
+
 print("****************************")
 print("Welcome to the guessing game")
 print("****************************")
 
-secret_number = 13
-round_total = 3
+secret_number = random.randrange(1, 101)
+points = 1000
 
-for current_round in range(1, round_total +1):
+print("Select the difficulty:\n(1) Easy (2) Normal (3) Hard")
+difficulty = int(input("Enter difficulty:"))
+
+if (difficulty == 1):
+    round_total = 20
+elif (difficulty == 2):
+    round_total = 10
+else:
+    round_total = 5
+
+for current_round in range(1, round_total + 1):
     print(f"Round {current_round} of {round_total}")
     user_input = int(input("Enter a number between 1 and 100: "))
 
-    if(user_input < 1 or user_input > 100):
+    if (user_input < 1 or user_input > 100):
         print("You should enter a number between 1 and 100.")
         continue
 
@@ -17,15 +31,18 @@ for current_round in range(1, round_total +1):
     higher_guess = user_input > secret_number
     lower_guess = user_input < secret_number
 
-    if(correct_guess):
+    if (correct_guess):
         print("Congratulations! You guessed correctly.")
+        print(f"You got {points} points")
         break
     else:
-        if(higher_guess):
+        if (higher_guess):
             print("Ooops! You guessed a higher number then the secret one.")
         else:
             print("Ooops! You guessed a lower number then the secret one.")
 
-print("The end")
+        if(current_round == round_total):
+            points = points - abs(secret_number - user_input)
 
-
+print(f"The secret number was {secret_number}")
+print(f"You got {points} points.")
